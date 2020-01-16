7575b266a6bfbbb11e6ae0425270d93e19d51ed3
Nicholas Nelson <nickandwolf@gmail.com>
2020-01-16 19:18:56 +0900

    Initial dumb shit

--- Diablo_Clone/main.c
+++ Diablo_Clone/main.c
@@ -11,44 +11,9 @@
 #include <string.h>
 #include <stdio.h>
 #include "raylib.h"
-#include "HUD.h"
-#include "Node.h"
-#include "Render.h"
-
-Camera2D camera;
-Node * NodeHead;
-Node * MainPlayer;
-//we could have a second NodeHead for map shit...
-
-const int TILESIZE = 48; //So collisions work, I guess
-
 
 int main(int argc, const char * argv[]) {
-    //ToggleFullscreen();
     
-    InitHeadNode();
-    CreatePlayer();
-    CreateNode(1);
-    MakeMap(TEST_LEVEL);
-    MergeSort(&NodeHead); //make a thingy saying if we should change this or not...
-    
-    InitRender();
-    InitHUD();
-    
-    HideCursor();
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(BLACK);
-        
-        RenderDraw();
-        RenderHUD();
-        
-        UpdateNode();
-        UpdateHUD();
-        RenderUpdate();
-        
-        EndDrawing();
-    }
     
     return 0;
 }
